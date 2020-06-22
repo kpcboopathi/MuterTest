f749f71165b9ac24ebbf47104f7db09cc386f361
Boopathi <kpcboopathi@gmail.com>
2020-06-22 08:32:33 +0530

    Initial commit

---
+++ ExampleApp/Module.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func areEqual(_ a: Int, and b: Int) -> Bool {
+        return a == b
+}
+
+func isGreater(_ a: Int) -> Bool {
+    return a >= 100
+}
---
+++ ExampleApp/Module2.swift
@@ -0,0 +1,8 @@
+func shouldReturnTrue() -> Bool {
+    return true && false
+}
+
+
+func shouldReturnTrue1() -> Bool {
+    return true && false
+}
